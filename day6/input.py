05a84a5e4f3653e09354a45ea57b1232347beba3
rollingball211 <77780100+rollingball211@users.noreply.github.com>
2021-01-28 16:46:58 +0900

    Add files via upload

---
+++ day6/input.py
@@ -0,0 +1,6 @@
+a = input() #입력되는 모든것을 문자열로 취급
+print(a)
+
+#프롬프트 띄워서 입력 받기.
+number = input("숫자를 입력하세요: ")
+print(number)
---
+++ day6/print.py
@@ -0,0 +1,7 @@
+#큰따옴표로 둘러싸인 문자열은 +연산과 동일함
+print("life" "is" "tooshort")
+#문자열 띄어쓰기는 ,로 함.
+print("life","is","too short")
+#한 줄에 결과값 출력하기.
+for i in range(10):
+    print(i,end=' ')
